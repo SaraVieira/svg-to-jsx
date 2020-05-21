a6d48bd657a9629af79401b905cfeeee3e13c9f5
Sara Vieira <hey@iamsaravieira.com>
2020-05-21 22:05:44 +0200

    pride is what I am

--- api/index.js
+++ api/index.js
@@ -1,64 +1,57 @@
-const express = require('express')
-const helmet = require('helmet')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const svgr = require('@svgr/core')
-const port = 3000
-require('@svgr/plugin-svgo')
-require('@svgr/plugin-prettier')
-require('@svgr/plugin-jsx')
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const svgr = require("@svgr/core");
+const port = 3000;
+require("@svgr/plugin-svgo");
+require("@svgr/plugin-prettier");
+require("@svgr/plugin-jsx");
 
-const app = express()
-app.use(helmet())
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   const {
     name,
     svg,
     native,
-    ext = 'js',
+    ext = "js",
     icon,
-    dimensions,
-    expandProps = 'end',
-    ref,
-    replaceAttrValues,
-    svgProps,
-    titleProp
-  } = req.body
+    expandProps = "end",
+    typescript,
+  } = req.body;
   try {
+    console.log(native, svg);
     const data = svgr.default.sync(
       svg,
       {
         native,
         ext,
         icon,
-        dimensions,
         expandProps,
-        ref,
-        replaceAttrValues,
-        svgProps,
-        titleProp,
+        typescript,
         plugins: [
-          '@svgr/plugin-svgo',
-          '@svgr/plugin-jsx',
-          '@svgr/plugin-prettier'
-        ]
+          "@svgr/plugin-svgo",
+          "@svgr/plugin-jsx",
+          "@svgr/plugin-prettier",
+        ],
       },
-      { componentName: name || 'Icon' }
-    )
-    console.log('data', data)
+      { componentName: name || "Icon" }
+    );
+    console.log("data", data);
     res.send({
-      data
-    })
+      data,
+    });
   } catch (e) {
-    console.log('error', e)
+    console.log("error", e);
     res.status(500).send({
-      error: "Couldn't parse SVG"
-    })
+      error: "Couldn't parse SVG",
+    });
   }
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,13 @@
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf))
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-figma.currentPage.selection.map(selected => {
-  selected.exportAsync({ format: 'SVG' }).then(svgCode => {
-    const svg = ab2str(svgCode)
-    figma.showUI(__html__, { visible: false })
-    figma.ui.postMessage({ type: 'networkRequest', data: svg })
-    figma.ui.onmessage = async msg => {
-      figma.showUI(__html__, { visible: true })
-      figma.ui.postMessage({ type: 'show-ui', data: msg })
-    }
-  })
-})
+figma.currentPage.selection.map((selected) => {
+  selected.exportAsync({ format: "SVG" }).then((svgCode) => {
+    const svg = ab2str(svgCode);
+    figma.showUI(__html__, { visible: false });
+    figma.ui.postMessage({ type: "networkRequest", data: svg });
+    figma.showUI(__html__, { visible: true, width: 500, height: 500 });
+    figma.ui.postMessage({ type: "show-ui", data: svg });
+  });
+});
