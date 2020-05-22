05f9c645332f07b4e68129b8e42389abcd1a7345
Sara Vieira <hey@iamsaravieira.com>
2020-05-22 14:57:14 +0200

    use figma-ds

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,45 @@
-const CopyPlugin = require('copy-webpack-plugin')
-const path = require('path')
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = (env, argv) => ({
-  mode: argv.mode === 'production' ? 'production' : 'development',
+  mode: argv.mode === "production" ? "production" : "development",
 
   // This is necessary because Figma's 'eval' works differently than normal eval
-  devtool: argv.mode === 'production' ? false : 'inline-source-map',
+  devtool: argv.mode === "production" ? false : "inline-source-map",
 
   entry: {
-    code: './src/code.ts' // The entry point for your plugin code
+    code: "./src/code.ts",
   },
 
   module: {
     rules: [
       // Converts TypeScript code to JavaScript
-      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
       {
         test: /\.css$/,
-        loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        loader: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
       {
         test: /\.(png|jpg|gif|webp|svg|zip)$/,
-        loader: [{ loader: 'url-loader' }]
-      }
-    ]
+        loader: [{ loader: "url-loader" }],
+      },
+    ],
   },
 
-  // Webpack tries these extensions for you if you omit the extension like "import './file'"
-  resolve: { extensions: ['.tsx', '.ts', '.jsx', '.js'] },
+  resolve: { extensions: [".ts", ".js"] },
 
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist') // Compile into a folder called "dist"
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
 
-  // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
-    new CopyPlugin([
-      {
-        from: './src/ui.html'
-      }
-    ])
-  ]
-})
+    new CopyPlugin({
+      patterns: [{ from: "./src/ui.html" }],
+    }),
+  ],
+});
