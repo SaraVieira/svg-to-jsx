212a205308be041dd40f2775d97bfe66a0fa0eb6
neicore <neicoreadams@gmail.com>
2023-05-03 12:41:00 +0300

    add ability to detect selection change and generate new svg

--- src/code.ts
+++ src/code.ts
@@ -1,13 +1,50 @@
+// Utility functions start here
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
+  return String.fromCharCode.apply(null, new Uint16Array(buf))
 }
 
+const debounceSelection = (func, wait, immediate) => {
+  let timeout
+  return function () {
+    let context = this,
+      args = arguments
+    let later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    let callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
+const exportSelected = (svgCode: Uint8Array) => {
+  const svg = ab2str(svgCode)
+  figma.showUI(__html__, { visible: true, width: 500, height: 500 })
+  figma.ui.postMessage({ type: 'networkRequest', data: svg })
+}
+
+// Figma functionalities start here
+
 figma.currentPage.selection.map((selected) => {
-  selected.exportAsync({ format: "SVG" }).then((svgCode) => {
-    const svg = ab2str(svgCode);
-    figma.showUI(__html__, { visible: false });
-    figma.ui.postMessage({ type: "networkRequest", data: svg });
-    figma.showUI(__html__, { visible: true, width: 500, height: 500 });
-    figma.ui.postMessage({ type: "show-ui", data: svg });
-  });
-});
+  selected.exportAsync({ format: 'SVG' }).then((svgCode) => {
+    exportSelected(svgCode)
+  })
+})
+
+//@ts-ignore (figma has updated their plugin API and the types in this project are outdated)
+figma.on(
+  'selectionchange',
+  debounceSelection(
+    () => {
+      figma.currentPage.selection.map((selected) => {
+        selected.exportAsync({ format: 'SVG' }).then((svgCode) => {
+          exportSelected(svgCode)
+        })
+      })
+    },
+    1000,
+    0
+  )
+)
