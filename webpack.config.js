e4f2c52537aa776a104949430b1fde2f43f9c08e
Sara Vieira <hey@iamsaravieira.com>
2024-03-05 12:19:29 +0000

    update to packages and compabiklity with dynamic page loading

--- webpack.config.js
+++ webpack.config.js
@@ -17,16 +17,16 @@ module.exports = (env, argv) => ({
       { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      {
-        test: /\.css$/,
-        loader: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      // },
 
-      // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      {
-        test: /\.(png|jpg|gif|webp|svg|zip)$/,
-        loader: [{ loader: "url-loader" }],
-      },
+      // // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
+      // {
+      //   test: /\.(png|jpg|gif|webp|svg|zip)$/,
+      //   loader: [{ loader: "url-loader" }],
+      // },
     ],
   },
 
